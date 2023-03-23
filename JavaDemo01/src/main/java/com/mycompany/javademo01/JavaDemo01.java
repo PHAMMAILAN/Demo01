09ed546854ac004413c0a4b19d180d978469f8ec
Mai Lan <phammailan@gmail.com>
2023-03-24 05:55:52 +0700

    First

---
+++ JavaDemo01/src/main/java/com/mycompany/javademo01/JavaDemo01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javademo01;
+
+/**
+ *
+ * @author ngoth
+ */
+public class JavaDemo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
